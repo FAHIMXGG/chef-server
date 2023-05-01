d6257ae7dc20c19eb4190c658f86991c2237f227
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-05-01 21:04:43 +0600

    data setup

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+
+const news = require('./data/chef.json')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Dragon is running')
+})
+// app.get('/categories', (req, res) => {
+//   res.send(categories)
+// })
+app.get('/chef', (req, res) => {
+  res.send(news)
+})
+
+// load by ID
+app.get('/chef/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedNews = news.find(n => n._id === id);
+  res.send(selectedNews);
+})
+
+// match with category ID in 2json
+// app.get('/categories/:id', (req, res) => {
+//   const id = parseInt(req.params.id);
+//   if (id == 0) {
+//     res.send(news)
+//   }
+//   else {
+//     const categoriesNews = news.filter(n => parseInt(n.category_id) === id);
+//     res.send(categoriesNews);
+//   }
+
+// })
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
